80a4b3d0f797ea156136cd57d606f72c0157b170
Mackilem <mack.vdl@gmail.com>
2014-10-27 14:00:56 -0200

    inserido a criação do usuário

--- script_parts/inicio.sh
+++ script_parts/inicio.sh
@@ -12,4 +12,8 @@ apt-get install language-pack-pt --yes
 locale-gen pt_BR.UTF-8
 update-locale LANG=pt_BR.UTF-8
 
+echo "Criando e configurando o usuário"
+
+adduser --system --home $DIR_PADRAO --group --shell /bin/bash $USUARIO
+
 
