c12c04cf0e8d4c04cc5db2e9bda0144f54c4fcdf
Mackilem <mack.vdl@gmail.com>
2014-10-27 12:59:06 -0200

    apontando para os scripts certos

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 export DIR_PADRAO=/home/openerp/v7
-export USUARIO=openerp
-export USUARIO_BD='openerp'
-export SENHA_BD='12345'
+export USUARIO=
+export USUARIO_BD=''
+export SENHA_BD=''
 
-./script/ajustes_finais.sh
+./script_parts/inicio.sh
+./script_parts/postgresql.sh
+./script_parts/dependencias.sh
+./script_parts/repositorios.sh
+./script_parts/conclusao.sh
