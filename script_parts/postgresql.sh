5480cbb48a793778ba90ac6c6f751f01238bc1cf
Mackilem <mack.vdl@gmail.com>
2014-10-27 14:06:17 -0200

    inserido as variáveis

--- script_parts/postgresql.sh
+++ script_parts/postgresql.sh
@@ -4,10 +4,10 @@ echo "Instalando banco de dados"
 apt-get install postgresql --yes
 
 echo "Criando usuario"
-su -u postgres createuser --superuser openerp
+su postgres createuser --superuser $USUARIO_BD
 
 echo "Configurando o Postgres"
-su -u postgres psql -c"ALTER user openerp WITH PASSWORD '1234'"
+su postgres psql -c"ALTER user $USUARIO_BD WITH PASSWORD $SENHA_BD"
 
 #mv /opt/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 #mv /opt/postgresql.conf /etc/postgresql/9.1/main/postgresql.conf
