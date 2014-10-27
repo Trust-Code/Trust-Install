7f9c4aeeb7c343e15f5d29ee78e1b321889ee7aa
Mackilem <mack.vdl@gmail.com>
2014-10-27 14:29:24 -0200

    inserido a condição para atualizar

--- script_parts/inicio.sh
+++ script_parts/inicio.sh
@@ -2,11 +2,11 @@
 
 cd /
 
-echo "Iniciando a atualização da lista de pacotes"
-apt-get update
-
-echo "Iniciando a atualização"
-apt-get upgrade --yes
+if ["$ATUALIZAR" = "sim"]
+	echo "Atualizando o sistema"
+	apt-get update
+	apt-get upgrade --yes
+fi
 
 apt-get install language-pack-pt --yes
 locale-gen pt_BR.UTF-8
