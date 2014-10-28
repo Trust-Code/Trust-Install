24aeaae15bd69c1c4f3f9c47225f49be7f67f54e
Mackilem <mack.vdl@gmail.com>
2014-10-28 13:54:36 -0200

    ajustado para instalar a versão 9.3 e criar o usuário

--- script_parts/postgresql.sh
+++ script_parts/postgresql.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+echo ">>> Instalando e atualizando o repositório oficial do POSTGRESQL <<<"
+cd /
+echo "# Repositório Oficial do Postgresql" >> etc/apt/sources.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> etc/apt/sources.list
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
+
 echo "Instalando banco de dados"
-apt-get install postgresql --yes
+apt-get install postgresql-9.3 --yes
 
 echo "Criando usuario"
-su postgres createuser --superuser $USUARIO_BD
-
-echo "Configurando o Postgres"
-su postgres psql -c"ALTER user $USUARIO_BD WITH PASSWORD $SENHA_BD"
+su postgres << EOF
+	createuser --createdb --username postgres --no-createrole --no-superuser --no-password $USUARIO_BD
+#	createuser [-d -U postgres -R -S -w] $USUARIO
+	psql -c "ALTER USER $USUARIO_BD WITH PASSWORD '$SENHA_BD'" -d template1
+	exit
+EOF
 
 #mv /opt/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 #mv /opt/postgresql.conf /etc/postgresql/9.1/main/postgresql.conf
+
