423b848807bc41ab911e73f80acdfa58de39b174
Mackilem <mack.vdl@gmail.com>
2014-11-06 14:20:04 -0200

    inserida a instalação do mail_forward

--- script_parts/install_modulos.sh
+++ script_parts/install_modulos.sh
@@ -21,7 +21,7 @@ fi
 
 #>>> módulos a serem instalados <<<
 
-modulos=l10n_br,l10n_br_account,l10n_br_account_payment,l10n_br_account_payment_extension,l10n_br_account_product,l10n_br_base,l10n_br_crm,l10n_br_crm_zip,l10n_br_data_account,l10n_br_data_account_product,l10n_br_data_base,l10n_br_data_zip,l10n_br_delivery,l10n_br_product,l10n_br_sale,l10n_br_sale_stock,l10n_br_stock,l10n_br_zip,disable_openerp_online,html_signature,l10n_br_data_zip
+modulos=l10n_br,l10n_br_account,l10n_br_account_payment,l10n_br_account_payment_extension,l10n_br_account_product,l10n_br_base,l10n_br_crm,l10n_br_crm_zip,l10n_br_data_account,l10n_br_data_account_product,l10n_br_data_base,l10n_br_data_zip,l10n_br_delivery,l10n_br_product,l10n_br_sale,l10n_br_sale_stock,l10n_br_stock,l10n_br_zip,disable_openerp_online,html_signature,l10n_br_data_zip,contacts,forward_mail
 
 echo "Criando banco de dados: $nome_banco"
 
